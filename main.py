59e22213dbca191284e8d154c6b7de8c6513907a
amaya lim <amayalim99@gmail.com>
2024-08-26 19:16:05 -0700

    documentation

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import import_ipynb
 from ml import closest
+import pandas as pd
 
 def recommendation(p):
     return f"Output playlist: {rec}"
+
+def get_playlist_link(playlist):
+    playlists = pd.read_csv('csv_files/playlists.csv')
+    row = playlists.iloc['name' == playlist]
+    link = row['link']
+    return link
 
 if __name__ == "__main__":
     in_playlist = input("Input playlist: ")
